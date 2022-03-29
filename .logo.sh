fa34db3323712dd79eb479551e9005d4fcbbb637
Parixit Sutariya <32614362+Bhai4You@users.noreply.github.com>
2022-03-29 11:00:20 +0530

    Create .logo.sh

---
+++ .logo.sh
@@ -0,0 +1,9 @@
+ban4(){                                         clear
+toilet -t -f pagga  " S ! M h u n t  " -F border | lolcat
+echo "         Created by Parixit Sutariya"
+echo -e "\e[32m\e[1m                     "
+sleep 0.2
+echo
+}
+
+ban4

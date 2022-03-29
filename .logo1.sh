56e569764da51dfeb84ede6be5d689117d1e2a9e
Parixit Sutariya <32614362+Bhai4You@users.noreply.github.com>
2022-03-29 10:57:59 +0530

    Update and rename xenc to .logo1.sh

---
+++ .logo1.sh
@@ -0,0 +1,37 @@
+clear
+ban1(){
+echo -e "\e[32m\e[1m                     "
+sleep 0.2
+echo "                    \ /"
+sleep 0.2
+echo "                     V"
+sleep 0.1
+echo "                     |                  "
+sleep 0.2
+echo "                     |                 "
+sleep 0.2
+echo "                     |               "
+sleep 0.2
+toilet -t -f pagga  " S ! M h u n t  " -F border | lolcat
+echo "         Created by Parixit Sutariya"
+echo -e "\e[32m\e[1m                     "
+sleep 0.2
+echo "                     |"
+sleep 0.1
+echo "                     |                  "
+sleep 0.2
+echo "                     |                 "
+sleep 0.5
+echo "                     |----Name" | lolcat
+sleep 0.7
+echo "             City----|" | lolcat
+sleep 0.8
+echo "                     |----Location" | lolcat
+sleep 0.2
+echo "                     |"
+sleep 0.1
+echo -e "                     v\e[0m"
+sleep 0.8
+}
+
+ban1
